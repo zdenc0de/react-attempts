0b1e9dcc7f33b158d2f50c8497e7aba85ccffca7
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-21 23:59:21 -0600

    TanStack

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { MyRoutes } from "./routers/router.jsx";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
-    <MyRoutes />
+    <QueryClientProvider client={queryClient}>
+      <MyRoutes />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/pages/ApisPage.jsx
+++ src/pages/ApisPage.jsx
@@ -1,31 +1,72 @@
+import { useQuery } from "@tanstack/react-query"
 import { useState, useEffect } from "react"
+import axios from "axios"
 
 export const ApisPage = () => {
     const [pokemons, setPokemons] = useState([])
+    const [cargando, setCargando] = useState(true)
+    const {data, isLoading, error} = useQuery({
+        queryKey: ['consulta a pokeapi'],
+        queryFn: async() => {
+            const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
+            return res.data.results
+        }
+    })
+    if (isLoading) return (
+        <span>
+            Cargando...
+        </span>
+    )
+``
+    if (error) return (
+        <span>
+            Error: {error.message}
+        </span>
+    )
+
+    useEffect(() => {
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
+        .then((res) => {
+            setPokemons(res.data.results)
+            setCargando(false)
+            console.log("data", res)
+        })
+        .catch ((err) => {
+            console.error("Error fetching data:", err)
+            setCargando(false)
+        })
+
+    }, [])
+
+    {/*
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
         .then((res) => res.json())
         .then((data) => {
             setPokemons(data.results)
+            setCargando(false)
             console.log("data", data.results)
+        }).catch((error) => {
+            console.error("Error fetching data:", error)
+            setCargando(false)
         })
-
     }, [])
+    */}
+
     return (
-        <div
-        className="h-screen bg-amber-300 text-black">
-               <span>
-                   ApisPage
-               </span>
-               <section className="flex flex-col">
-               {
-                pokemons?.map((item, index) => (
-                    <span 
-                    key={index}>
-                        {item.name}
-                    </span>
-                ))
-               }
+        <div className="h-screen bg-amber-300 text-black">
+            <span>
+                ApisPage
+            </span>
+            <section 
+            className="flex flex-col">
+                {data?.map((item, index) => {
+                    return (
+                        <span key={index}>
+                            {item.name}
+                        </span>
+                    )
+                })}
                </section>
         </div>
     )
