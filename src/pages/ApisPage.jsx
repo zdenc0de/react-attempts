979586d8b59ad8f67d8723ef630d83d1af070afa
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-21 19:06:14 -0600

    Apis attempt

--- src/pages/ApisPage.jsx
+++ src/pages/ApisPage.jsx
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react"
+
 export const ApisPage = () => {
+    const [pokemons, setPokemons] = useState([])
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+        .then((res) => res.json())
+        .then((data) => {
+            setPokemons(data.results)
+            console.log("data", data.results)
+        })
+
+    }, [])
     return (
         <div
         className="h-screen bg-amber-300 text-black">
                <span>
                    ApisPage
                </span>
+               <section className="flex flex-col">
+               {
+                pokemons?.map((item, index) => (
+                    <span 
+                    key={index}>
+                        {item.name}
+                    </span>
+                ))
+               }
+               </section>
         </div>
     )
 }
