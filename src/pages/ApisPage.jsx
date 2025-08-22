31871240053d91e60766902051c9d814e1ce318b
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-22 13:52:14 -0600

    Media skills added

--- src/pages/ApisPage.jsx
+++ src/pages/ApisPage.jsx
@@ -3,12 +3,14 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 export const ApisPage = () => {
-    const {data, isLoading, error} = useQuery({
+    const {data, isLoading, error, refetch} = useQuery({
         queryKey: ['consulta a pokeapi'],
         queryFn: async() => {
             const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
             return res.data.results
-        }
+        },
+        refetchOnWindowFocus: false, // Prevent refetching on window focus
+        refetchInterval: 10000, // Refetch data every 10 seconds
     })
     if (isLoading) return (
         <span>
@@ -22,26 +24,16 @@ export const ApisPage = () => {
         </span>
     )
 
-    {/*
-    useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
-        .then((res) => res.json())
-        .then((data) => {
-            setPokemons(data.results)
-            setCargando(false)
-            console.log("data", data.results)
-        }).catch((error) => {
-            console.error("Error fetching data:", error)
-            setCargando(false)
-        })
-    }, [])
-    */}
-
     return (
         <div className="h-screen bg-amber-300 text-black">
             <span>
                 ApisPage
             </span>
+            <button 
+            className="bg-black text-white"
+            onClick={() => refetch()}>
+                Click a refetch
+            </button>
             <section 
             className="flex flex-col">
                 {data?.map((item, index) => {
