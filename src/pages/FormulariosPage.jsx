71dbf88de48eff4e0dc202cbaf8f786e37247994
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-23 15:16:22 -0600

    Text errors

--- src/pages/FormulariosPage.jsx
+++ src/pages/FormulariosPage.jsx
@@ -1,8 +1,13 @@
 import { useForm } from "react-hook-form"
 export const FormulariosPage = () => {
-    const { register, handleSubmit } = useForm()
+    const { 
+        register,
+         handleSubmit, 
+         formState: { errors },
+         } = useForm()
     const enviar = (data) => {
-        console.log(data)
+
+        alert(data.nombre)
     }
     return (
         <main className="h-screen bg-amber-300 text-black flex flex-col">
@@ -10,11 +15,20 @@ export const FormulariosPage = () => {
                 Formularios Page
             </h1>
             <form onSubmit={handleSubmit(enviar)}>
-                <input {
-                    ...register("nombre")
-                }
-                className="p-2 border" 
-                placeholder="Nombre" />
+                <input
+                    {...register("nombre", {
+                        required: "El nombre es obligatorio",
+                        minLength: {
+                            value: 3,
+                            message: "Debe tener al menos 3 caracteres"
+                        }
+                    })}
+                    className="p-2 border"
+                    placeholder="Nombre"
+                />
+                <p>
+                    {errors.nombre?.message}
+                </p>
                 <button
                 type="submit"
                 className="p-2 bg-black text-white ml-2 hover:bg-gray-800">
