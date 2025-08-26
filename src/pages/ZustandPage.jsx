dca58d7a6aeb33d0d7a55bffdfadd861ea530abd
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-26 09:47:43 -0600

    Reset Button

--- src/pages/ZustandPage.jsx
+++ src/pages/ZustandPage.jsx
@@ -1,7 +1,7 @@
 import { useContadorStore } from "../store/ContadorStore";
 
 export const ZustandPage = () => {
-    const {incrementar, contador} = useContadorStore();
+    const {incrementar, contador, resetear} = useContadorStore();
     return (
         <div
         className="h-screen flex flex-col bg-amber-300 text-black gap-5 p-4">
@@ -9,9 +9,13 @@ export const ZustandPage = () => {
                className="text-2xl font-bold">
                     Contador: {contador}
                </span>
-               <button onClick={() => incrementar(contador)}
+               <button onClick={incrementar}
                className="bg-black text-white p-2 rounded-lg">
                     Incrementar 
+               </button>
+               <button onClick={resetear}
+               className="bg-black text-white p-2 rounded-lg">
+                    Resetear 
                </button>
         </div>
     )
--- src/store/ContadorStore.jsx
+++ src/store/ContadorStore.jsx
@@ -2,5 +2,6 @@ import { create } from "zustand";
 
 export const useContadorStore = create((set) => ({
     contador: 0, 
-    incrementar: (p) => set((state) => ({contador: state.contador + 1}))
+    incrementar: (p) => set((state) => ({contador: state.contador + 1})), 
+    resetear: () => set({contador: 0})
 }))
