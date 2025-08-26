e66dc9cddadc21c08574c48fb55c93c3c17d8748
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-26 09:53:54 -0600

    Persist

--- src/store/ContadorStore.jsx
+++ src/store/ContadorStore.jsx
@@ -1,7 +1,14 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useContadorStore = create((set) => ({
-    contador: 0, 
-    incrementar: (p) => set((state) => ({contador: state.contador + 1})), 
-    resetear: () => set({contador: 0})
-}))
+export const useContadorStore = create (
+    persist(
+        (set) => ({
+            contador: 0, 
+            incrementar: () => set((state) => ({contador: state.contador + 1})),
+            resetear: () => set({contador: 0})
+        }), {
+            name: "contador-store"
+        }
+    )
+)
