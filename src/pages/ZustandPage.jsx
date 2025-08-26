fedd4e9934dec0f57d022c53235e603679aa5bf6
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-25 23:53:35 -0600

    Zustand counter

--- src/pages/ZustandPage.jsx
+++ src/pages/ZustandPage.jsx
@@ -1,10 +1,18 @@
+import { useContadorStore } from "../store/ContadorStore";
+
 export const ZustandPage = () => {
+    const {incrementar, contador} = useContadorStore();
     return (
         <div
-        className="h-screen bg-amber-300 text-black">
-               <span>
-                   ZustandPage
+        className="h-screen flex flex-col bg-amber-300 text-black gap-5 p-4">
+               <span
+               className="text-2xl font-bold">
+                    Contador: {contador}
                </span>
+               <button onClick={() => incrementar(contador)}
+               className="bg-black text-white p-2 rounded-lg">
+                    Incrementar 
+               </button>
         </div>
     )
 }
---
+++ src/store/ContadorStore.jsx
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+
+export const useContadorStore = create((set) => ({
+    contador: 0, 
+    incrementar: (p) => set((state) => ({contador: state.contador + 1}))
+}))
